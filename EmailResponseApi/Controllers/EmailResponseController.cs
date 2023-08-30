dbc27e483c2d533eb90991e0b201e19faa3e0744
beyonddeepak <deepak.sharma@beyondkey.com>
2023-08-30 22:55:32 +0530

    .

--- EmailResponseApi/Controllers/EmailResponseController.cs
+++ EmailResponseApi/Controllers/EmailResponseController.cs
@@ -3,9 +3,13 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace EmailResponseApi.Controllers
@@ -25,7 +29,9 @@ namespace EmailResponseApi.Controllers
         {
             try
             {
-                var apiKey = "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";
+                // Remove additional white spaces from the input text
+                inputText = Regex.Replace(inputText, @"\s+", " ").Trim();
+                var apiKey = "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";
                 var endpoint = "https://api.openai.com/v1/chat/completions";
 
                 using (var client = new HttpClient())
@@ -33,21 +39,22 @@ namespace EmailResponseApi.Controllers
                     var request = new HttpRequestMessage(HttpMethod.Post, endpoint);
                     request.Headers.Add("Authorization", $"Bearer {apiKey}");
 
-                    var jsonBody = @"{
-                ""model"": ""gpt-3.5-turbo"",
-                ""messages"": [ 
-                    {
-                        ""role"": ""system"",
-                        ""content"": ""You are a helpful assistant that generates formal responses of 3 to 5 lines in proper formating and indenting.""
-                    },
-                    {
-                        ""role"": ""user"",
-                        ""content"": """ + inputText.Replace("\"", "\\\"") + @"""
-                    }
-                ]
-            }";
+                    var customInstruction = "You are an assistant that should understand user query and should provides information as responder from Beyond key Systems (https://www.beyondkey.com) in 3 to 5 lines and should use information like their services, technologies, solutions, career opportunities, about company, insights, locations etc. for better result should visit Beyond Key website at https://www.beyondkey.com.";
 
-                    request.Content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                    var jsonBody = new
+                    {
+                        model = "gpt-3.5-turbo",
+                        messages = new[]
+                        {
+                            new { role = "system", content = customInstruction },
+                            new { role = "user", content = inputText }
+                        }
+                    };
+
+                    var jsonBodyString = JsonConvert.SerializeObject(jsonBody);
+
+                    request.Content = new StringContent(jsonBodyString, Encoding.UTF8, "application/json");
 
                     var response = await client.SendAsync(request);
                     var responseContent = await response.Content.ReadAsStringAsync();
@@ -56,14 +63,13 @@ namespace EmailResponseApi.Controllers
                     {
                         throw new Exception($"Error calling OpenAI API: {response.StatusCode} - {responseContent}");
                     }
-
-
-
+                    var finalresponse = Regex.Replace(responseContent, @"(As an AI assistant,|As an AI language model,)", "", RegexOptions.IgnoreCase).Trim();
                     var customResponse = new CustomResponse
                     {
                         StatusCode = response.StatusCode,
-                        Content = responseContent
+                        Content = finalresponse
                     };
+
                     return customResponse;
                 }
             }
@@ -77,5 +83,8 @@ namespace EmailResponseApi.Controllers
                 };
             }
         }
+
+
+
     }
 }
